72becebdbf727cca6dc47dbe6c9d46860377d42d
zwu002 <zwu002@gold.ac.uk>
2017-03-03 17:05:41 +0000

    First communication through Arduino! Used two buttons to move the sprite up and down in Unity.

---
+++ ArduinoSpaceShooter/Arduino - Space Shooter/Assets/Scripts/MoveShip.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class MoveShip : MonoBehaviour {
+
+    public float speed = 1;
+    float amountToMove;
+
+    SerialPort sp = new SerialPort("COM4", 9600);
+
+    void Start () {
+        sp.Open();
+        sp.ReadTimeout = 1;
+	}
+	
+	void Update () {
+        amountToMove = speed * Time.deltaTime;
+
+        if (sp.IsOpen)
+        {
+            try
+            {
+                MoveObject(sp.ReadByte());
+                print(sp.ReadByte());
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+	}
+
+    void MoveObject(int direction)
+    {
+        if (direction == 1)
+        {
+            transform.Translate(Vector2.up * amountToMove, Space.World);
+        }
+        else if (direction == 2)
+        {
+            transform.Translate(Vector2.down * amountToMove, Space.World);
+        }
+    }
+}
