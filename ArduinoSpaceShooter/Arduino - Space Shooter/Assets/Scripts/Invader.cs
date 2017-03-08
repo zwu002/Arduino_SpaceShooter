ad38e4c245b6a6d60b8e1770a46d036856c6befb
zwu002 <zwu002@gold.ac.uk>
2017-03-08 18:10:13 +0000

    New low pixel sprites imported

---
+++ ArduinoSpaceShooter/Arduino - Space Shooter/Assets/Scripts/Invader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Invader : MonoBehaviour {
+
+    public float invaderSpeed;
+    Rigidbody2D rb;
+
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        transform.Translate(Vector2.left * invaderSpeed, Space.World);
+    }
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+
+    }
+}
+
