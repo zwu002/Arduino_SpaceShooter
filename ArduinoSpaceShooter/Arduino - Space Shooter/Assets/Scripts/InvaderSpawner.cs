d905ede26ec1fff4a64bbf080494dafde593ecb1
zwu002 <zwu002@gold.ac.uk>
2017-03-08 18:32:37 +0000

    Invader spawner setup, random spawning enemies between 0.1 - 2 seconds

--- ArduinoSpaceShooter/Arduino - Space Shooter/Assets/Scripts/InvaderSpawner.cs
+++ ArduinoSpaceShooter/Arduino - Space Shooter/Assets/Scripts/InvaderSpawner.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class InvaderSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject spawnSprite;
+    public float minPos = -2.6f;
+    public float maxPos = 2.6f;
+    public float xPos = 4.5f;
+    float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Instantiate(spawnSprite, new Vector2(xPos, Random.Range(minPos, maxPos)), Quaternion.identity);
+            timer = Random.Range(0.2f, 2f);
+        }
+    }
 }
