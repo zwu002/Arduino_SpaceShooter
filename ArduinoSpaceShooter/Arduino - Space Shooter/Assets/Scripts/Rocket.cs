89d3ec435860514d77b42ac064e3237b12c20202
zwu002 <zwu002@gold.ac.uk>
2017-03-15 16:03:46 +0000

    Moving rockets and score text

---
+++ ArduinoSpaceShooter/Arduino - Space Shooter/Assets/Scripts/Rocket.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket : MonoBehaviour {
+    
+    public float rocketSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.right * rocketSpeed, Space.World);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Border")
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (col.gameObject.tag == "Enemy")
+        {
+
+        }
+    }
+
+}
---
+++ ArduinoSpaceShooter/Arduino - Space Shooter/Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Text scoreText;
+    public int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "Score: " + score;
+    }
+}
